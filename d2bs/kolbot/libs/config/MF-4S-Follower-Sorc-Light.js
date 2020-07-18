98ea25e43cf3a6564649e51ee1eaf9dfb303e7c9
Roger-That <roger.takeshita@gmail.com>
2020-07-17 20:23:32 -0400

    Updte char config, add UseTelekinesis and TownCheck
        - Config.UseTelekinesis
            - Use telekinesis to pick up potions, keys...
        - Config.TownCheck
            - Set true to go to town and buy if out of potions
        - Config.GoToTownHP
        - Config.GoToTownMP
            - Set true to go to town and buy if out of potions
            - Only available to RogerThat-Master config file, because normal bot uses TownCheck

--- d2bs/kolbot/libs/config/MF-4S-Follower-Sorc-Light.js
+++ d2bs/kolbot/libs/config/MF-4S-Follower-Sorc-Light.js
@@ -78,29 +78,29 @@ function LoadConfig() {
       Config.StashGold    = 100000;
    //! TOWN SETTINGS / POTION SETTINGS / CHICKEN SETTINGS ------------------------------
       //+ Town settings -------------------------------------------------------------
-         Config.HealHP        = 90;                      // Go to a healer if under designated percent of life.
-         Config.HealMP        =  0;                      // Go to a healer if under designated percent of mana.
-         Config.HealStatus    = true;                    // Go to a healer if poisoned or cursed
-         Config.UseMerc       = true;                    // Use merc. This is ignored and always false in d2classic.
-         Config.MercWatch     = true;                    // Instant merc revive during battle.
+         Config.HealHP       = 90;                       // Go to a healer if under designated percent of life.
+         Config.HealMP       =  0;                       // Go to a healer if under designated percent of mana.
+         Config.HealStatus   = true;                     // Go to a healer if poisoned or cursed
+         Config.UseMerc      = true;                     // Use merc. This is ignored and always false in d2classic.
+         Config.MercWatch    = true;                     // Instant merc revive during battle.
       //+ Potion settings -----------------------------------------------------------
-         Config.UseHP         = 85;                      // Drink a healing potion if life is under designated percent.
-         Config.UseRejuvHP    = 65;                      // Drink a rejuvenation potion if life is under designated percent.
-         Config.UseMP         = 40;                      // Drink a mana potion if mana is under designated percent.
-         Config.UseRejuvMP    = 10;                      // Drink a rejuvenation potion if mana is under designated percent.
-         Config.UseMercHP     = 85;                      // Give a healing potion to your merc if his/her life is under designated percent.
-         Config.UseMercRejuv  = 40;                      // Give a rejuvenation potion to your merc if his/her life is under designated percent.
-         Config.HPBuffer      =  0;                      // Number of healing potions to keep in inventory.
-         Config.MPBuffer      =  0;                      // Number of mana potions to keep in inventory.
-         Config.RejuvBuffer   =  0;                      // Number of rejuvenation potions to keep in inventory.
+         Config.UseHP        = 85;                       // Drink a healing potion if life is under designated percent.
+         Config.UseRejuvHP   = 65;                       // Drink a rejuvenation potion if life is under designated percent.
+         Config.UseMP        = 40;                       // Drink a mana potion if mana is under designated percent.
+         Config.UseRejuvMP   = 10;                       // Drink a rejuvenation potion if mana is under designated percent.
+         Config.UseMercHP    = 85;                       // Give a healing potion to your merc if his/her life is under designated percent.
+         Config.UseMercRejuv = 40;                       // Give a rejuvenation potion to your merc if his/her life is under designated percent.
+         Config.HPBuffer     =  0;                       // Number of healing potions to keep in inventory.
+         Config.MPBuffer     =  0;                       // Number of mana potions to keep in inventory.
+         Config.RejuvBuffer  =  0;                       // Number of rejuvenation potions to keep in inventory.
+         Config.TownHP       = 55;                       // Go to town if life is under designated percent.
+         Config.TownMP       = 10;                       // Go to town if mana is under designated percent.
+         Config.TownCheck    = true;                     // Go to town if out of potions
       //+ Chicken settings ----------------------------------------------------------
-         Config.LifeChicken   = 35;                      // Exit game if life is less or equal to designated percent.
-         Config.ManaChicken   =  0;                      // Exit game if mana is less or equal to designated percent.
-         Config.MercChicken   =  0;                      // Exit game if merc's life is less or equal to designated percent.
-         Config.TownHP        = 55;                      // Go to town if life is under designated percent.
-         Config.TownMP        = 10;                      // Go to town if mana is under designated percent.
-         Config.GoToTownHP    = false;                   // Go to town if out of healing potions
-         Config.GoToTownMP    = false;                   // Go to town if out of mana potions
+         Config.LifeChicken  = 35;                       // Exit game if life is less or equal to designated percent.
+         Config.ManaChicken  =  0;                       // Exit game if mana is less or equal to designated percent.
+         Config.MercChicken  =  0;                       // Exit game if merc's life is less or equal to designated percent.
+         Config.PingQuit     = [{Ping: 0, Duration: 0}]; // Quit if ping is over the given value for over the given time period in seconds.
    //! PICK LIST -----------------------------------------------------------------------
       // Config.PickitFiles.push("w-bot1.nip");
       Config.PickitFiles.push("w-bot2.nip");
@@ -114,25 +114,23 @@ function LoadConfig() {
       Config.PickitFiles.push("w-LLD.nip");
       Config.PickRange = 40;
       Config.FastPick  = true;
-   //! IN GAME CONFIG ------------------------------------------------------------------
+   //! MANUAL PLAY / OPEN CHEST --------------------------------------------------------
       Config.ManualPlayPick = false;
       Config.OpenChests = false;                         // Open chests. Controls key buying. true = open only chests, 2 = open everything body, rocks...
       Config.AutoMap = false;                            // Set to true to open automap at the beginning of the game.
    //! GENERAL CONFIG ------------------------------------------------------------------
-      Config.LastMessage = "";                           // Message or array of messages to say at the end of the run. Use $nextgame to say next game - "Next game: $nextgame" (works with lead entry point)
       Config.MinGameTime = 0;                            // Min game time in seconds. Bot will TP to town and stay in game if the run is completed before.
       Config.MaxGameTime = 1080;                         // Maximum game time in seconds. Quit game when limit is reached.
-      Config.TeleSwitch = false;                         // Switch to secondary (non-primary) slot when teleporting more than 5 nodes.
+      Config.Silence     = false;                        // Make the bot not say a word. Do not use in combination with LocalChat
+      Config.LastMessage = "";                           // Message or array of messages to say at the end of the run. Use $nextgame to say next game - "Next game: $nextgame" (works with lead entry point)
+      Config.TeleSwitch  = false;                        // Switch to secondary (non-primary) slot when teleporting more than 5 nodes.
       Config.MiniShopBot = true;                         // Scan items in NPC shops.
-      Config.TownCheck = false;                          // Go to town if out of potions
-      Config.LogExperience = false;                      // Print experience statistics in the manager.
-      Config.PingQuit = [{Ping: 0, Duration: 0}];        // Quit if ping is over the given value for over the given time period in seconds.
-      Config.Silence = false;                            // Make the bot not say a word. Do not use in combination with LocalChat
       Config.ScanShrines = [15, 2, 3];                   // 15 = XP, 2 = Health, 3 = Mana
-      Config.MFSwitchPercent = 10;                       // Boss life % to switch to non-primary weapon slot. Set to 0 to disable.
-      Config.PrimarySlot = -1;                           // Set to use specific weapon slot as primary weapon slot: -1 = disabled, 0 = slot I, 1 = slot II
       Config.WalkIfManaLessThan = 10;
-   //! FATMOD CONFIG -----------------------------------------------------------------
+   //! MF SWITCH -----------------------------------------------------------------------
+      Config.MFSwitchPercent = 10;                       // Boss life % to switch to non-primary weapon slot. Set to 0 to disable.
+      Config.PrimarySlot     =  1;                       // Set to use specific weapon slot as primary weapon slot: -1 = disabled, 0 = slot I, 1 = slot II
+   //! FASTMOD CONFIG ------------------------------------------------------------------
       Config.PacketShopping = true;                      // Use packets to shop. Improves shopping speed.
       Config.FCR = 255;                                  // 0 - disable, 1 to 255 - set value of faster cast rate
       Config.FHR = 255;                                  // 0 - disable, 1 to 255 - set value of faster hit recovery
@@ -140,6 +138,7 @@ function LoadConfig() {
       Config.IAS = 255;                                  // 0 - disable, 1 to 255 - set value of increased attack speed
       Config.PacketCasting = 2;                          // 0 = disable, 1 = packet teleport, 2 = full packet casting.
       Config.WaypointMenu = true;
+      Config.UseTelekinesis = true;
    //! ADDITION CONFIG -----------------------------------------------------------------
       //+ Print to console ----------------------------------------------------------
          Config.D2BotPrintGameTime    = false;           // Game time in the end of each game
@@ -148,6 +147,7 @@ function LoadConfig() {
          Config.D2BotPrintScriptError = false;           // Script error
          Config.D2BotPrintDie         =  true;           // When you die
          Config.D2BotPrintLostXp      = false;           // When you lose experience
+         Config.LogExperience         = false;           // Print experience statistics in the manager.
       //+ Additional item info log settings. All info goes to \logs\ItemLog.txt -----
          Config.ItemInfo = false;                        // Log stashed, skipped (due to no space) or sold items.
          Config.ItemInfoQuality = [];                    // The quality of sold items to log. See NTItemAlias.dbl for values. Example: Config.ItemInfoQuality = [6, 7, 8];
