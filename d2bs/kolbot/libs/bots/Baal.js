8de846cf89090db81c59b65bb113f151333a5ca0
Roger Takeshita <roger.takeshita@gmail.com>
2021-03-21 21:11:24 -0700

    style: Should fix indetation

--- d2bs/kolbot/libs/bots/Baal.js
+++ d2bs/kolbot/libs/bots/Baal.js
@@ -11,55 +11,55 @@ function Baal() {
 		var check;
 
 		switch (me.classid) {
-		case 1: // Sorceress
-			switch (Config.AttackSkill[3]) {
-			case 49:
-			case 53:
-			case 56:
-			case 59:
-			case 64:
-				if (me.getState(121)) {
-					while (me.getState(121)) {
-						delay(100);
-					}
-				} else {
-					return Skill.cast(Config.AttackSkill[1], 0, 15094 + rand(-1, 1), 5028);
+			case 1: // Sorceress
+				switch (Config.AttackSkill[3]) {
+					case 49:
+					case 53:
+					case 56:
+					case 59:
+					case 64:
+						if (me.getState(121)) {
+							while (me.getState(121)) {
+								delay(100);
+							}
+						} else {
+							return Skill.cast(Config.AttackSkill[1], 0, 15094 + rand(-1, 1), 5028);
+						}
+
+						break;
 				}
 
 				break;
-			}
+			case 3: // Paladin
+				if (Config.AttackSkill[3] === 112) {
+					if (Config.AttackSkill[4] > 0) {
+						Skill.setSkill(Config.AttackSkill[4], 0);
+					}
 
-			break;
-		case 3: // Paladin
-			if (Config.AttackSkill[3] === 112) {
-				if (Config.AttackSkill[4] > 0) {
-					Skill.setSkill(Config.AttackSkill[4], 0);
+					return Skill.cast(Config.AttackSkill[3], 1);
 				}
 
-				return Skill.cast(Config.AttackSkill[3], 1);
-			}
-
-			break;
-		case 5: // Druid
-			if (Config.AttackSkill[3] === 245) {
-				return Skill.cast(Config.AttackSkill[3], 0, 15094 + rand(-1, 1), 5028);
-			}
-
-			break;
-		case 6: // Assassin
-			if (Config.UseTraps) {
-				check = ClassAttack.checkTraps({x: 15094, y: 5028});
-
-				if (check) {
-					return ClassAttack.placeTraps({x: 15094, y: 5028}, 5);
+				break;
+			case 5: // Druid
+				if (Config.AttackSkill[3] === 245) {
+					return Skill.cast(Config.AttackSkill[3], 0, 15094 + rand(-1, 1), 5028);
 				}
-			}
 
-			if (Config.AttackSkill[3] === 256) { // shock-web
-				return Skill.cast(Config.AttackSkill[3], 0, 15094, 5028);
-			}
+				break;
+			case 6: // Assassin
+				if (Config.UseTraps) {
+					check = ClassAttack.checkTraps({ x: 15094, y: 5028 });
 
-			break;
+					if (check) {
+						return ClassAttack.placeTraps({ x: 15094, y: 5028 }, 5);
+					}
+				}
+
+				if (Config.AttackSkill[3] === 256) { // shock-web
+					return Skill.cast(Config.AttackSkill[3], 0, 15094, 5028);
+				}
+
+				break;
 		}
 
 		return false;
@@ -72,23 +72,23 @@ function Baal() {
 			do {
 				if (Attack.checkMonster(monster) && monster.y < 5080) {
 					switch (monster.classid) {
-					case 23:
-					case 62:
-						return 1;
-					case 105:
-					case 381:
-						return 2;
-					case 557:
-						return 3;
-					case 558:
-						return 4;
-					case 571:
-						return 5;
-					default:
-						Attack.getIntoPosition(monster, 10, 0x4);
-						Attack.clear(15);
+						case 23:
+						case 62:
+							return 1;
+						case 105:
+						case 381:
+							return 2;
+						case 557:
+							return 3;
+						case 558:
+							return 4;
+						case 571:
+							return 5;
+						default:
+							Attack.getIntoPosition(monster, 10, 0x4);
+							Attack.clear(15);
 
-						return false;
+							return false;
 					}
 				}
 			} while (monster.getNext());
@@ -226,9 +226,9 @@ function Baal() {
 		this.announce();
 		Pather.moveTo(15118, 5002);
 		Pather.makePortal();
-    if (Config.Baal.HotTPMessage !== "") {
-      say(Config.Baal.HotTPMessage);
-    }
+		if (Config.Baal.HotTPMessage !== "") {
+			say(Config.Baal.HotTPMessage);
+		}
 		Attack.clear(15);
 	}
 
@@ -237,9 +237,9 @@ function Baal() {
 	if (Config.PublicMode) {
 		Pather.moveTo(15118, 5045);
 		Pather.makePortal();
-    if (Config.Baal.SafeTPMessage !== "") {
-      say(Config.Baal.SafeTPMessage);
-    }
+		if (Config.Baal.SafeTPMessage !== "") {
+			say(Config.Baal.SafeTPMessage);
+		}
 		Precast.doPrecast(true);
 	}
 
